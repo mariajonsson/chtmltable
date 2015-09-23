cc20442601fb1a13b8fb30911a4c7bdbaa777794
mariajonsson <maria.jonsson@tam-arkiv.se>
2015-09-23 18:44:05 +0200

    test files

---
+++ test/HTMLTable/SimpleHTMLTableTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Meax\HTMLTable;
+
+class SimpleHTMLTableTest extends \PHPUnit_Framework_TestCase
+{
+
+  /**
+  * Creates an HTML table from an array or object.
+  *
+  */
+  
+  }
