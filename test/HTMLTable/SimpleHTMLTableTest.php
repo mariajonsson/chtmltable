6b5de6f8c0fcf8bcad3e15d604d7e2f85d81bb7c
mariajonsson <maria.jonsson@tam-arkiv.se>
2015-09-23 19:12:12 +0200

    autoloader and test

--- test/HTMLTable/SimpleHTMLTableTest.php
+++ test/HTMLTable/SimpleHTMLTableTest.php
@@ -2,12 +2,26 @@
 
 namespace Meax\HTMLTable;
 
-class SimpleHTMLTableTest extends \PHPUnit_Framework_TestCase
-{
-
-  /**
+ /**
   * Creates an HTML table from an array or object.
   *
   */
+
+class SimpleHTMLTableTest extends \PHPUnit_Framework_TestCase
+{
+
+ 
+  /**
+ * Test 
+ *
+ * @return void
+ *
+ */
+  public function testCreateTable()
+  {
+  
+  $tbl = new \Meax\HTMLTable\SimpleHTMLTable('test');
+  
+  }
   
   }
