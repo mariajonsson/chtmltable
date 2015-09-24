8e56f3ce6fe068eaeb7e7015d42a3653ebcff380
mariajonsson <maria.jonsson@tam-arkiv.se>
2015-09-24 19:32:02 +0200

    adding assertions

--- test/HTMLTable/SimpleHTMLTableTest.php
+++ test/HTMLTable/SimpleHTMLTableTest.php
@@ -17,10 +17,24 @@ class SimpleHTMLTableTest extends \PHPUnit_Framework_TestCase
  * @return void
  *
  */
-  public function testCreateTable()
+  public function testCreateTable_th()
   {
   
   $tbl = new \Meax\HTMLTable\SimpleHTMLTable('test');
+  
+  $table = $tbl->createTable(array([ 'name' => 'fruits', 'label' => 'Fruits'], ['name' => 'animals', 'label' => 'Animals']),null,"tablename");
+  $this->assertEquals( $table, "<table id='tablename'><tr><th>Fruits</th><th>Animals</th></tr></table>" );
+  
+ 
+  
+  }
+  public function testCreateTable_thtd()
+  {
+  
+  $tbl = new \Meax\HTMLTable\SimpleHTMLTable('test');
+  
+  $table = $tbl->createTable(array([ 'name' => 'fruits', 'label' => 'Fruits'], ['name' => 'animals', 'label' => 'Animals']), array(['fruits' => 'Apple', 'animals' => 'Horse'], ['fruits' => 'Banana', 'animals' => 'Monkey']));
+  $this->assertEquals( $table, "<table id=''><tr><th>Fruits</th><th>Animals</th></tr><tr><td>Apple</td><td>Horse</td></tr><tr><td>Banana</td><td>Monkey</td></tr></table>" );
   
   }
   
