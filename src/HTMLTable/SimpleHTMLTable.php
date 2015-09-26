6b77d85c8ed12473d85d079d72a2f954491a3fbb
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-09-26 17:25:52 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/HTMLTable/SimpleHTMLTable.php
+++ src/HTMLTable/SimpleHTMLTable.php
@@ -5,22 +5,22 @@ namespace Meax\HTMLTable;
 class SimpleHTMLTable {
 
   /**
-  * Creates an HTML table from an array or object.
-  *
-  */
+   * Creates an HTML table from an array or object.
+   *
+   */
   
   public function __construct() {
   }
 
     /**
-    * Build an html table
-    *
-    * @param array $columns containing label and corresponding name
-    * @param array $values or @param object $values containing the data
-    * 
-    */
+     * Build an html table
+     *
+     * @param array $columns containing label and corresponding name
+     * @param array $values or @param object $values containing the data
+     * 
+     */
 
-  public function createTable($columns, $values=null, $tablename=null) {
+  public function createTable($columns, $values = null, $tablename = null) {
 
     $html = "<table id='".$tablename."'>";
     $html .= "<tr>";
@@ -35,37 +35,37 @@ class SimpleHTMLTable {
       $html .= "<tr>";
       
       foreach ($columns as $column) {
-	$linkkey = null;
-	$val = '';
-	if (is_object($value)) {
-	  if(isset($value->{$column['name']})) {
-	    $val = $value->{$column['name']};
-	  }
-	  if (isset($column['linkkey'])) {
-	    if (isset($value->{$column['linkkey']})) {
-	      $linkkey = $value->{$column['linkkey']};
-	    }
-	  }
-	}
-	elseif (is_array($value)) {
-	  if(isset($value[$column['name']])) {
-	    $val = $value[$column['name']];
-	  }
-	  if (isset($column['linkkey'])) {
-	    $linkkey = $value[$column['linkkey']];
-	  }
-	}
-	if (isset($column['display']) && isset($column['displayformat'])) {
-	  $val = $this->getDisplayVal($val, $column['display'], $column['displayformat']);
-	}
-	elseif (isset($column['display'])) {
-	  $val = $this->getDisplayVal($val, $column['display']);
-	}
+  $linkkey = null;
+  $val = '';
+  if (is_object($value)) {
+    if(isset($value->{$column['name']})) {
+      $val = $value->{$column['name']};
+    }
+    if (isset($column['linkkey'])) {
+      if (isset($value->{$column['linkkey']})) {
+        $linkkey = $value->{$column['linkkey']};
+      }
+    }
+  }
+  elseif (is_array($value)) {
+    if(isset($value[$column['name']])) {
+      $val = $value[$column['name']];
+    }
+    if (isset($column['linkkey'])) {
+      $linkkey = $value[$column['linkkey']];
+    }
+  }
+  if (isset($column['display']) && isset($column['displayformat'])) {
+    $val = $this->getDisplayVal($val, $column['display'], $column['displayformat']);
+  }
+  elseif (isset($column['display'])) {
+    $val = $this->getDisplayVal($val, $column['display']);
+  }
 
-	$link = (isset($column['linkbase'])) ? '<a href="' .$column['linkbase'].$linkkey.'">' : null;
-	$endlink = !empty($link) ? "</a>" : null;
-	$html .= "<td>".$link.$val.$endlink."</td>";
-	} 
+  $link = (isset($column['linkbase'])) ? '<a href="' .$column['linkbase'].$linkkey.'">' : null;
+  $endlink = !empty($link) ? "</a>" : null;
+  $html .= "<td>".$link.$val.$endlink."</td>";
+  } 
       $html .= "</tr>";
      
       }
@@ -76,37 +76,37 @@ class SimpleHTMLTable {
   }
   
     /**
-    * Support for converting values 
-    *
-    * @param $val the value to convert
-    * @param string $displaytype determines which switch case to go with
-    * @param string $format which format to convert to, if any
-    */
+     * Support for converting values 
+     *
+     * @param $val the value to convert
+     * @param string $displaytype determines which switch case to go with
+     * @param string $format which format to convert to, if any
+     */
 	
-  public function getDisplayVal($val, $displaytype=null, $format=null) {
+  public function getDisplayVal($val, $displaytype = null, $format = null) {
   
   switch ($displaytype) {
     
-  case 'yes-no':
-    if ($val === false || $val === 0 || empty($val) || $val === null) {
-      $displayval = "No";
-    }
-    else { 
-      $displayval = "Yes";
-    }
-    break;
-  case 'convert-datestr':
-    if (isset($format)) {
-      $displayval = date($format, strtotime($val));
-    }
-    else {
-      $displayval = $val;
-    }
-    break;
+    case 'yes-no':
+      if ($val === false || $val === 0 || empty($val) || $val === null) {
+        $displayval = "No";
+      }
+      else { 
+        $displayval = "Yes";
+      }
+      break;
+    case 'convert-datestr':
+      if (isset($format)) {
+        $displayval = date($format, strtotime($val));
+      }
+      else {
+        $displayval = $val;
+      }
+      break;
   
-  default:
-    $displayval = $val;
-  break;
+    default:
+      $displayval = $val;
+    break;
   }
   return $displayval;
   }
--- test/config.php
+++ test/config.php
@@ -1,3 +1,3 @@
 <?php
 
-include __DIR__ . "/../autoloader.php";
+include __DIR__."/../autoloader.php";
